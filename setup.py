f1cac2b882e52604510d3215d85d4a66c71065e9
Abtin Gramian <abtin.gramian@gmail.com>
2015-09-28 12:37:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@
 from distutils.core import setup
 
 setup(name='custom_text_test_runner',
-      version='1.0',
+      version='0.1',
       description='Python Custom Text Test Runner',
       author='Abtin Gramian',
       author_email='abtin.gramian@gmail.com',
       url='https://github.com/agramian/custom-text-test-runner',
       packages=['custom_text_test_runner'],
       install_requires=['table_printer'],
-      dependency_links=['https://github.com/agramian/table-printer/tarball/master#egg=table_printer-1.0']
+      download_url = 'https://github.com/agramian/custom-text-test-runner/tarball/v0.1',
+      keywords = ['unittest', 'custom', 'test', 'runner', 'result', 'json'],
+      classifiers = [],
      )
