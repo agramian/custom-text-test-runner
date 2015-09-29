670606bc720f9b38b83a512331d43abb079380a6
Abtin Gramian <abtin.gramian@gmail.com>
2015-09-29 13:35:50 -0700

    bumped version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
     read_md = lambda f: open(f, 'r').read()
 
 setup(name='custom_text_test_runner',
-      version='0.1.2',
+      version='0.1.3',
       description='Python Custom Text Test Runner',
       long_description=read_md('README.md'),
       author='Abtin Gramian',
@@ -21,7 +21,7 @@ setup(name='custom_text_test_runner',
       url='https://github.com/agramian/custom-text-test-runner',
       packages=['custom_text_test_runner'],
       install_requires=['table_printer'],
-      download_url = 'https://github.com/agramian/custom-text-test-runner/tarball/v0.1.2',
+      download_url = 'https://github.com/agramian/custom-text-test-runner/tarball/v0.1.3',
       keywords = ['unittest', 'custom', 'test', 'runner', 'result', 'json'],
       classifiers = [],
      )
